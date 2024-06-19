c3dbbd0babfe2d7042da94b44dc8a7f723605b69
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-06-19 14:40:26 +0530

    chore: changed font to poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 
-const font = Open_Sans({ subsets: ["latin"] });
+const font = Poppins({ subsets: ["latin"], weight: ["400"]});
 
 export const metadata: Metadata = {
   title: "Discord App",
