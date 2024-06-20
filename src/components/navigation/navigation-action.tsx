42fd3b34c1982890dd1a33011a26ef2ab612ec7c
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-06-20 17:08:15 +0530

    feat: show modal for adding a server

--- src/components/navigation/navigation-action.tsx
+++ src/components/navigation/navigation-action.tsx
@@ -2,12 +2,14 @@
 
 import { Plus } from "lucide-react";
 import { ActionToolTip } from "../action-tooltip";
+import { useModal } from "../../../hooks/use-modal-store";
 
 export const NavigationAction = () => {
+  const {onOpen} = useModal();
   return (
     <div>
       <ActionToolTip label="Add a server" side="right" align="center">  
-      <button className="group flex items-center">
+      <button className="group flex items-center" onClick={() => onOpen("createServer")}>
         <div className="flex h-[48px] w-[48px] mx-3 rounded-[24px] group-hover:rounded-[16px] bg-background dark:bg-neutral-700 group-hover:bg-emerald-500 items-center justify-center overflow-hidden transition-all">
             <Plus
               className="group-hover:text-white transition text-emerald-500"
