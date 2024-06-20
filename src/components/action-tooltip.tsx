010cf01114045e86f779dfe9c03dcafc0b52670f
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-06-20 15:28:22 +0530

    chore: tooltip component to show server name on hover

---
+++ src/components/action-tooltip.tsx
@@ -0,0 +1,23 @@
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
+
+interface ActionToolTipProps{
+    label: string;
+    children: React.ReactNode;
+    side?: "top" | "bottom" | "right" | "left";
+    align?: "start" | "center" | "end";
+}
+
+export const ActionToolTip = ({label, children, side, align}: ActionToolTipProps) => {
+    return (
+        <TooltipProvider>
+            <Tooltip delayDuration={50}>
+                <TooltipTrigger asChild>{children}</TooltipTrigger>
+                <TooltipContent side={side} align={align}>
+                    <p className="font-semibold text-sm capitalize">
+                        {label.toLowerCase()}
+                    </p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+}
