c4c04ee0f529fd5dcbfe4e4b37a5474a7c3de9a9
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-06-30 22:47:16 +0530

    chore: show user avatar in chat header component

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,6 +1,6 @@
 import { Hash, Menu } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
-import { channel } from "diagnostics_channel";
+import { UserAvatar } from "../user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -20,6 +20,9 @@ export const ChatHeader = ({
       <MobileToggle serverId={serverId}/>
       {type === "channel" && (
         <Hash className="size-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="h-8 w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">
         {name}
