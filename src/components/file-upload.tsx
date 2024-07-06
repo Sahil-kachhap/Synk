736bce14dd854e10488a35af3c8d38c30d04259c
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-07-07 00:12:46 +0530

    feat: support for file attachements while sending messages (including pdf)

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -3,7 +3,7 @@
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 import { error } from "console";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 const FileUpload = ({
@@ -31,6 +31,18 @@ const FileUpload = ({
         </button>
       </div>
     );
+  }
+
+  if(value && fileType === "pdf"){
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="size-10 fill-indigo-100 stroke-indigo-400"/>
+        <a href={value} target="_blank" rel="noopener noreferrer" className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline">{value}</a>
+        <button onClick={() => onChange("")} className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm" type="button">
+          <X className="size-4" />
+        </button>
+      </div>
+    )
   }
 
   return (
