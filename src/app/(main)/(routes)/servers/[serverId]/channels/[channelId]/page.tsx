e73c76c79fd08c06e94c527933ee6e45a36c52bb
Sahil Kachhap <Sahil.kachhap111989@gmail.com>
2024-07-10 11:25:28 +0530

    feat: create component to write, read chat messages on any channel of a server

---
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,74 @@
+import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
+import { currentProfile } from "@/lib/curent-profile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIDPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIDPage = async ({ params }: ChannelIDPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !profile) {
+    redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        serverId={channel.serverId}
+        name={channel.name}
+        type="channel"
+      />
+      <ChatMessages
+        member={member!}
+        chatId={channel.id}
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
+      <ChatInput
+        type="channel"
+        name={channel.name}
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
+};
+
+export default ChannelIDPage;
